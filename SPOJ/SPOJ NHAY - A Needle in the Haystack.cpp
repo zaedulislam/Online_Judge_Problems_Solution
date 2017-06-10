7d4253e71531ee4a0b3eeca4fff7e04c6f0d1d2d
Zayed Islam <khzayed1@gmail.com>
2017-06-10 19:39:07 +0600

    Add files via upload

---
+++ SPOJ/SPOJ NHAY - A Needle in the Haystack.cpp
@@ -0,0 +1,156 @@
+// SPOJ NHAY - A Needle in the Haystack.cpp
+
+/// Template by Zayed ///
+
+///************************************************************///
+/// #include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+
+#include <string>
+#include <cstring>
+#include <sstream>
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+//#include<unordered_map>
+#include <set>
+//#include<unordered_set>
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+///************************************************************///
+using namespace std;
+int caseno = 1;
+///************************************************************///
+#define NL '\n'
+#define SF scanf
+#define PF printf
+#define PC() printf("Case %d: ", caseno++)//NOTES:printf
+//#define PC cout << "Case "//NOTES:cout
+//#define CN cout << caseno++ << ": "//NOTES:cout
+#define CLR(ar) memset(ar, 0, sizeof(ar))
+#define SET(ar) memset(ar, -1, sizeof(ar))
+#define READ() freopen("input.txt", "r", stdin)
+#define WRITE() freopen("output.txt", "w", stdout)
+#define BOOST std::ios_base::sync_with_stdio(0);
+///************************************************************///
+typedef long long LL;//NOTES:"%lld"
+typedef unsigned long long ULL;//NOTES:"%llu"
+///************************************************************///
+#define INF (1 << 31) - 1
+#define MOD 1000000007
+#define PRIME 999998727899999 			// (largest prime below 10^16)
+#define PB push_back
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define pci pair<char, int>
+#define pLL pair<LL, LL>
+#define pis pair<int, string>
+#define psi pair<string, int>
+#define pss pair<string, string>
+#define PI 2 * acos(0.0);
+#define EPS 1e-11;
+///************************************************************///
+// Numeric Functions
+template < class T > inline void SWAP(T &a, T &b) { T t = a; a = b; b = t; }
+inline LL POW(LL base, LL power){
+	LL res = base; if (power == 0) return 1;
+	for (int I = 0; I < power - 1; I++) res *= base; return res;
+}
+// Translator Functions
+int ToInt(string s) { int r = 0; istringstream sin(s); sin >> r; return r; }//NOTES:ToInt(
+double ToDouble(string s) { double r = 0; istringstream sin(s); sin >> r; return r; }//NOTES:ToDouble(
+string ToString(int n) { string s; stringstream convert; convert << n; s = convert.str(); return s; }//NOTES:ToString(
+///************************************************************///
+
+/*
+*******4 Direction Array*******
+int dx[] = {0, 0, - 1, 1}, dy[] = {-1, 1, 0, 0};
+*******8 Direction Array*******
+int dx[] = {0, 0, -1, +1, -1, -1, +1, +1}, dy[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+********Knight Moves********
+int dx[] = {-2, -2, -1, -1, +1, +1, +2, +2}, dy[] = {-1, +1, -2, +2, -2, +2, -1, +1};
+*/
+
+const int SIZE = 1e6;
+char str[SIZE], pattern[SIZE];
+vector<LL> vi;
+
+
+int main()
+{
+	///BOOST
+	///READ();
+	///WRITE();
+	int T, I, J, K, N, n, m, cnt = 0, len;
+
+	LL hashno = 0, p = 57, p_pow = 1, mod = PRIME;
+	for(I = 0; I < SIZE; I++)
+	{
+		vi.PB(p_pow);
+		p_pow = ((p_pow % mod) * (p % mod)) % mod;
+	}
+
+
+	while(SF("%d", &n) != EOF)
+	{
+		SF("%s", pattern);
+		SF("%s", str);
+
+		if(strlen(pattern) > strlen(str))
+		{
+			PF("\n");
+			continue;
+		}
+
+
+
+		/// Calculate hash value for needle
+        hashno = 0, p = 57, p_pow = 1, mod = PRIME;
+        for(I = 0; I < n; I++)
+		{
+			hashno = ((hashno % mod) + ((pattern[I] - 'a' + 1) * vi[n - I - 1]) % mod) % mod;
+		}
+
+		LL h1 = hashno;
+
+
+		/// Calculate hash value for haystack
+        hashno = 0, p = 57, p_pow = 1, mod = PRIME;
+        for(I = 0; I < n; I++)
+		{
+			hashno = ((hashno % mod) + ((str[I] - 'a' + 1) * vi[n - I - 1]) % mod) % mod;
+		}
+
+		if(h1 == hashno)
+			PF("%d\n", 0);
+
+
+		len = strlen(str);
+		for(I = n; I < len; I++)
+		{
+			hashno = ((((hashno % mod) * vi[1]) % mod) - (((str[I - n] - 'a' + 1) * vi[n]) % mod) + mod) % mod;
+			hashno = (hashno + ((str[I] - 'a' + 1) * vi[0]) % mod) % mod;
+
+			if(hashno == h1)
+				PF("%d\n", I - n + 1);
+
+		}
+
+		PF("\n");
+
+	}
+
+
+	return 0;
+}
+
+
+
