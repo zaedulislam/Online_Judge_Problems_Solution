3936366de69ba05552cc4f9c88f82ff84cc3b209
Zayed Islam <khzayed1@gmail.com>
2017-06-11 16:26:28 +0600

    Add files via upload

---
+++ Codeforces/Codeforces 697A - Pineapple Incident.cpp
@@ -0,0 +1,136 @@
+// Codeforces 697A - Pineapple Incident.cpp
+
+/// Template by Zayed ///
+
+///************************************************************///
+/// #include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+
+#include <string>
+#include <cstring>
+#include <sstream>
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+//#include<unordered_map>
+#include <set>
+//#include<unordered_set>
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+///************************************************************///
+using namespace std;
+int caseno = 1;
+///************************************************************///
+#define NL '\n'
+#define SF scanf
+#define PF printf
+#define PC() printf("Case %d: ", caseno++)//NOTES:printf
+//#define PC cout << "Case "//NOTES:cout
+//#define CN cout << caseno++ << ": "//NOTES:cout
+#define CLR(ar) memset(ar, 0, sizeof(ar))
+#define SET(ar) memset(ar, -1, sizeof(ar))
+#define READ() freopen("input.txt", "r", stdin)
+#define WRITE() freopen("output.txt", "w", stdout)
+#define BOOST std::ios_base::sync_with_stdio(0);
+///************************************************************///
+typedef long long LL;//NOTES:"%lld"
+typedef unsigned long long ULL;//NOTES:"%llu"
+///************************************************************///
+#define INF (1 << 31) - 1
+#define MOD 1000000007
+#define PRIME 999998727899999 			// (largest prime below 10^16)
+#define PB push_back
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define pci pair<char, int>
+#define pLL pair<LL, LL>
+#define pis pair<int, string>
+#define psi pair<string, int>
+#define pss pair<string, string>
+#define PI 2 * acos(0.0);
+#define EPS 1e-11;
+///************************************************************///
+// Numeric Functions
+template < class T > inline void SWAP(T &a, T &b) { T t = a; a = b; b = t; }
+inline LL POW(LL base, LL power){
+	LL res = base; if (power == 0) return 1;
+	for (int I = 0; I < power - 1; I++) res *= base; return res;
+}
+// Translator Functions
+int ToInt(string s) { int r = 0; istringstream sin(s); sin >> r; return r; }//NOTES:ToInt(
+double ToDouble(string s) { double r = 0; istringstream sin(s); sin >> r; return r; }//NOTES:ToDouble(
+string ToString(int n) { string s; stringstream convert; convert << n; s = convert.str(); return s; }//NOTES:ToString(
+///************************************************************///
+
+/*
+*******4 Direction Array*******
+int dx[] = {0, 0, - 1, 1}, dy[] = {-1, 1, 0, 0};
+*******8 Direction Array*******
+int dx[] = {0, 0, -1, +1, -1, -1, +1, +1}, dy[] = {-1, +1, 0, 0, -1, +1, -1, +1};
+********Knight Moves********
+int dx[] = {-2, -2, -1, -1, +1, +1, +2, +2}, dy[] = {-1, +1, -2, +2, -2, +2, -1, +1};
+*/
+
+const int SIZE = 1e5 + 10;
+
+bool BinarySearch(int t, int s, int x)
+{
+	int first = 1, last = x;
+	while(first <= last)
+	{
+		LL mid = (first + last) / 2;
+
+		LL val1 = t + mid * s;
+		LL val2 = t + mid * s + 1;
+
+		if(val1 == x || val2 == x)
+			return 1;
+		else if(val1 < x)
+			first = mid + 1;
+		else
+			last = mid - 1;
+
+	}
+
+	return 0;
+
+}
+
+
+
+int main()
+{
+	BOOST
+	///READ();
+	///WRITE();
+	int T, I, J, K, N, n, m, cnt = 0, len, t, s, x;
+
+	cin >> t >> s >> x;
+
+	if(t == x)
+	{
+		cout << "YES\n";
+		return 0;
+	}
+
+	int ret = BinarySearch(t, s, x);
+	if(ret == 1)
+		cout << "YES\n";
+	else
+		cout << "NO\n";
+
+
+	return 0;
+}
+
+
+
+
