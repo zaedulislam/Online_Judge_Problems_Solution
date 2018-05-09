0d2da40c1b32c6edf73904f15235ef5721de8b31
Zaedul Islam <islam.zaedul@gmail.com>
2018-05-10 01:13:44 +0600

    Update UVa 369 - Combinations.cpp

--- UVa Online Judge/UVa 369 - Combinations.cpp
+++ UVa Online Judge/UVa 369 - Combinations.cpp
@@ -10,29 +10,29 @@
 #include<queue>
 typedef unsigned long long llu;
 using namespace std;
-    
+
 int main()
 {
-	int n, r,temp;
+	int I, n, r;
 	while (1) {
+
 		scanf("%d%d", &n, &r);
-		temp = r;
-		if (n == 0 && r == 0)
+
+		int m = r, N = n;
+        	if(n == 0 && r == 0)
 			break;
-		if (r == n - 1 || r == 1) {
-			printf("%d things taken %d at a time is %d exactly.\n", n, r, n);
-			continue;
-		}
-		if (r > (n >> 1))
+
+		if(n - r < r)
 			r = n - r;
 
-		int numerator, denominator = 1;
-		llu res = 1;
-		for (numerator = 0; numerator < r; numerator++) {
-			res = res * (n - numerator);
-			res = res / (denominator + numerator);
+		long long ans = 1;
+		for(I = 1; I <= r; I++, n--)
+		{
+			ans *= n;
+			ans /= I;
 		}
-		printf("%d things taken %d at a time is %llu exactly.\n", n, temp, res);
+
+		printf("%d things taken %d at a time is %lld exactly.\n", N, m, ans);
 	}
 
 	return 0;
