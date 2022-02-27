20df741a3092fa8ce945f70d5ac5eed3c16f1373
Zaedul Islam <islam.zaedul@gmail.com>
2022-02-27 13:05:56 -0500

    LeetCode 53. Maximum Subarray

---
+++ LeetCode/LeetCode 53. Maximum Subarray [O(N)].py
@@ -0,0 +1,16 @@
+'''
+    Time complexity: O(N)
+    Space complexity: O(1)
+'''
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        globalMaximum: int = nums[0]
+        localMaximum: int = nums[0] 
+            
+        for num in nums[1:]:
+            localMaximum = max(num, localMaximum + num)
+            globalMaximum = max(globalMaximum, localMaximum)
+            
+        return globalMaximum
