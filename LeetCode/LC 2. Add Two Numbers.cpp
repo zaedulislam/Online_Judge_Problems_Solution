e5dc8acedecef935151fd05374bc20cdc7fc9823
Zaedul Islam <islam.zaedul@gmail.com>
2022-09-26 02:41:35 -0400

    Create LC 2. Add Two Numbers.cpp

---
+++ LeetCode/LC 2. Add Two Numbers.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* sumHead = new ListNode(0);
+        ListNode* currentHead = sumHead;
+        
+        int carry = 0;
+        
+        while(l1 != NULL || l2 != NULL || carry > 0){
+            int a = l1 ? l1 -> val : 0;
+            int b = l2 ? l2 -> val : 0;
+            int sum = a + b + carry;
+            
+            carry = sum / 10;
+            
+            currentHead -> next = new ListNode(sum % 10);
+            currentHead = currentHead -> next;
+            
+            l1 = l1 ? l1 -> next : nullptr;
+            l2 = l2 ? l2 -> next : nullptr;
+        }
+        
+        return sumHead -> next;
+    }
+};
