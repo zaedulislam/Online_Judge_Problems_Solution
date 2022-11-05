c6e1a18b34f3ef201a3594f3e7aa099c6c31d96f
Zaedul Islam <islam.zaedul@gmail.com>
2022-11-05 14:01:55 -0400

    Update LC 1909. Remove One Element to Make the Array Strictly Increasing.cpp

--- LeetCode/LC 1909. Remove One Element to Make the Array Strictly Increasing.cpp
+++ LeetCode/LC 1909. Remove One Element to Make the Array Strictly Increasing.cpp
@@ -16,14 +16,19 @@ public:
             }
         }
         
-        
         if(count >= 2){
+            // If count is greater than one
             return false;
         }   else if(removeIndex == -1 ||  removeIndex == arrayLength - 1 || removeIndex == 1){
+                // If no element is removed
+                // If only the last or the
+                // first element is removed
                 return true;
         } else if(nums[removeIndex - 1] < nums[removeIndex + 1]){
+                // If a[index] is removed
                 return true;
         }   else if(removeIndex - 2 >= 0 && nums[removeIndex - 2] < nums[removeIndex]){
+                // If a[index - 1] is removed
                 return true;
         }
         
