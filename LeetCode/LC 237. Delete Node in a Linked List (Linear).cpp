0e736ee505c81219b781b4b220355f064c14f825
Zaedul Islam <islam.zaedul@gmail.com>
2023-01-08 02:21:30 -0500

    Create LC 237. Delete Node in a Linked List (Linear).cpp

---
+++ LeetCode/LC 237. Delete Node in a Linked List (Linear).cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        while(1){
+            node -> val = node -> next -> val;
+
+            if(node -> next -> next == NULL){
+                node -> next = NULL;
+                break;
+            }
+
+            node = node -> next;
+        }
+    }
+};
